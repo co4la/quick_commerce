57ec3306d61107c1fdeeb7f3ee80f3dc4f13bab2
Andrew Park <apark@movableink.com>
2017-05-02 23:50:55 -0400

    add seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,8 +1,14 @@
-10.times do
+user = User.create({
+  username: Faker::Name.name,
+  email: Faker::Internet.email,
+})
+
+100.times do
   Product.create!({
     name: Faker::Commerce.product_name,
     price: Faker::Commerce.price,
     description: Faker::Lorem.sentence(1),
-    image_url: Faker::Avatar.image
+    image_url: Faker::Avatar.image,
+    user_id: user.id
   })
 end
