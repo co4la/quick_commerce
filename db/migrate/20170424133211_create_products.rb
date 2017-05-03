d3a8a5d0ae920f1278ea80d6674a198b2d5b27e4
Andrew Park <apark@movableink.com>
2017-05-02 23:49:37 -0400

    add user and product model

--- db/migrate/20170424133211_create_products.rb
+++ db/migrate/20170424133211_create_products.rb
@@ -5,8 +5,10 @@ class CreateProducts < ActiveRecord::Migration[5.0]
       t.integer :price
       t.text :description
       t.string :image_url
+      t.integer :user_id
 
       t.timestamps
     end
+    add_index :products, :user_id
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170424133211) do
+ActiveRecord::Schema.define(version: 20170427022927) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -22,6 +22,15 @@ ActiveRecord::Schema.define(version: 20170424133211) do
     t.string   "image_url"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
+    t.integer  "user_id"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string   "username"
+    t.string   "password"
+    t.string   "email"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
 end
