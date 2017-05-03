24522bd5ac8e31d0deb50c5d1a1e5a51938a109d
Andrew Park <apark@movableink.com>
2017-05-02 23:51:45 -0400

    add shop image url for users

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170427022927) do
+ActiveRecord::Schema.define(version: 20170503034525) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -29,8 +29,9 @@ ActiveRecord::Schema.define(version: 20170427022927) do
     t.string   "username"
     t.string   "password"
     t.string   "email"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at",     null: false
+    t.datetime "updated_at",     null: false
+    t.string   "shop_image_url"
   end
 
 end
